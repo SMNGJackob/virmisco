800ca4ab6a3a460c08b6630955bc41e177e79506
sesogr <github-sesogr@sedna-soft.de>
2016-11-28 18:50:43 +0100

    Add script to start/stop existing docker containers

---
+++ docker-containers-stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+docker stop \
+    vmscgit-proxy-files \
+    vmscgit-proxy-mariadb \
+    vmscgit-proxy-mariadb-contao \
+    vmscgit-proxy-php-apache \
+    vmscgit-proxy-redis \
+    vmscgit-origin-files \
+    vmscgit-origin-mariadb \
+    vmscgit-origin-mariadb-contao \
+    vmscgit-origin-php-apache \
+    vmscgit-origin-redis
+
